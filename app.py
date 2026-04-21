97835bcb41ff0b5e865e5e47dc2d3a22e983bb37
Auribe18 <angel.uribe.10@gmail.com>
2026-04-21 17:04:55 -0400

    Se agrega sección de certificaciones y se generan en pdf final

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from reportlab.pdfgen import canvas
-from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, HRFlowable
 from reportlab.lib.pagesizes import A4
-from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
+from reportlab.lib.enums import TA_CENTER, TA_LEFT, TA_JUSTIFY
 from io import BytesIO
 import streamlit as st
 from streamlit_quill import st_quill
@@ -21,6 +22,7 @@ def main():
         teléfono = st.text_input("Teléfono")
         links = st.text_input("LinkedIn o Potafolio")
     direccion = st.text_input("Dirección")
+    perfil = st.text_area("Perfil")
 
     #Experiencia
     st.header("Experiencia Laboral")
@@ -86,6 +88,26 @@ def main():
         st.session_state.num_idioma += 1
         st.rerun()
 
+    #Contador para Certificaciones
+    st.header("Certificaciones")
+    if 'num_cert' not in st.session_state:
+        st.session_state.num_cert = 1
+    
+    #Campos para idioma
+    for i in range(st.session_state.num_cert):
+        with st.expander(f"Certificación #{i+1}", expanded=True):
+            col1, col2, col3 = st.columns(3)
+            with col1:
+                st.text_input(f"Certificación", key=f"cert_{i}")
+            with col2:
+                st.text_input(f"Institución", key=f"inst_{i}")
+            with col3:
+                st.date_input(f"Fecha", key=f"anio_cert_{i}",format="DD/MM/YYYY")
+   
+    if st.button("➕ Agregar Certificación"):
+        st.session_state.num_cert += 1
+        st.rerun()
+
     #Campos para habilidades
     habilidades = st.text_input("Habilidades (Separar con ,)")
 
@@ -99,34 +121,65 @@ def main():
         
     lista_idiomas = [{"Idioma": st.session_state[f"idioma_{i}"],"Nivel": st.session_state[f"nivel_{i}"]}
                          for i in range(st.session_state.num_idioma)]
+    
+    lista_certificaciones = [{"Certificación": st.session_state[f"cert_{i}"],"Institución": st.session_state[f"inst_{i}"], "Año": st.session_state[f"anio_cert_{i}"]}
+                             for i in range(st.session_state.num_cert)]
         
     buffer = BytesIO()
     doc = SimpleDocTemplate(buffer)
     styles = getSampleStyleSheet()
-    titulo = Paragraph(f'{nombre}', styles['Title'])
     elementos = []
-    elementos.append(Paragraph("EXPERIENCIA LABORAL", styles['Heading2']))
-        
+    
+    # Estilos personalizados para claridad
+    formato_nombre = ParagraphStyle('Nombre', parent=styles['Heading1'], alignment=TA_CENTER, fontSize=18, spaceAfter=5)
+    formato_contacto = ParagraphStyle('Contacto', parent=styles['Normal'], alignment=TA_CENTER, fontSize=10, spaceAfter=6)
+    formato_contenido = ParagraphStyle('Contenido', parent=styles['Normal'], alignment=TA_JUSTIFY, fontSize=10, spaceAfter=12)
+    formato_seccion = ParagraphStyle('Seccion', parent=styles['Heading2'], fontSize=12, spaceBefore=10, spaceAfter=1, borderPadding=2)
+
+    division = HRFlowable(width="100%", thickness=1, color="black", spaceAfter=10)
+    division_light = HRFlowable(width="100%", thickness=0.3, color="grey", spaceAfter=20)
+    
+    titulo = Paragraph(f'{nombre}', formato_nombre)
+    contacto = Paragraph(f"{direccion} | {email} | {teléfono} | {links}", formato_contacto)
+    perfil_profesional = Paragraph(f"{perfil}",formato_contenido)
+
+    elementos.append(titulo)
+    elementos.append(contacto)
+    elementos.append(division_light)
+
+    elementos.append(Paragraph("PERFIL", formato_seccion))
+    elementos.append(division)
+    elementos.append(perfil_profesional)
+    elementos.append(Spacer(1, 10))
+
+    elementos.append(Paragraph("EXPERIENCIA", formato_seccion))
+    elementos.append(division)  
     for item in lista_experiencia:
-        elementos.append(Paragraph(f"<b>{item['Puesto']}</b> en {item['Empresa']} | {item['Inicio']} - {item['Fin']}", styles['Normal']))
+        elementos.append(Paragraph(f"<b>{item['Puesto']}</b> en {item['Empresa']} | {item['Inicio']} - {item['Fin']}"))
         elementos.append(Spacer(1, 10))
-        elementos.append(Paragraph(f"{item['Tareas y logros']}"))
+        elementos.append(Paragraph(f"{item['Tareas y logros']}",formato_contenido))
         elementos.append(Spacer(1, 20))
 
-    elementos.append(Spacer(1, 15))
-    elementos.append(Paragraph("EDUCACIÓN", styles['Heading2']))
-
+    elementos.append(Paragraph("EDUCACIÓN", formato_seccion))
+    elementos.append(division)
     for item in lista_educacion:
-        elementos.append(Paragraph(f"<b>{item['Titulo']}</b> - {item['Institución']} - {item['Año']}", styles['Normal']))
+        elementos.append(Paragraph(f"<b>{item['Titulo']}</b> - {item['Institución']} - {item['Año']}"))
         elementos.append(Spacer(1, 10))   
-            
     elementos.append(Spacer(1, 15))
-    elementos.append(Paragraph("IDIOMAS", styles['Heading2']))
+
+    elementos.append(Paragraph("IDIOMAS", formato_seccion))
+    elementos.append(division)
     for item in lista_idiomas:
-        elementos.append(Paragraph(f"• {item['Idioma']}: {item['Nivel']}", styles['Normal']))
+        elementos.append(Paragraph(f"<b>{item['Idioma']}:</b> {item['Nivel']}"))
+
+    elementos.append(Paragraph("CERTIFICACIONES", formato_seccion))
+    elementos.append(division)
+    for item in lista_certificaciones:
+        elementos.append(Paragraph(f"<b>{item['Certificación']}</b> - {item['Institución']} - {item['Año']}"))
             
     doc.build(elementos)
     pdf_final = buffer.getvalue()
+
     if st.download_button(label="📥 Descargar CV PDF",data=pdf_final,file_name=f"CV_{nombre}.pdf",mime="application/pdf"):
         st.success(f"¡CV de {nombre} listo para descargar!")
 
